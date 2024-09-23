a165d574408c2ac3b98a05ec3bba3a28113f1d48
behnamhamidi1991 <eslp91@hotmail.com>
2024-09-23 10:36:09 +0330

    Refactoring

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route");
 const app = express();
+
+// *** Middleware ***
 
 // Use json
 app.use(express.json());
-
 // Use urlencoded form to post a product
 app.use(express.urlencoded({ extended: false }));
 
@@ -13,74 +14,14 @@ app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
 
+// routes
+app.use("/api/products", productRoute);
+
 // Index page
 app.get("/", (req, res) => {
   res.send("Hello there, this is my first nodejs rest api");
 });
 
-// Get all productions
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Post a production
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Update a product
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found!" });
-    }
-
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Delete a product
-app.delete("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const product = await Product.findByIdAndDelete(id);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not founrd!" });
-    }
-
-    res.status(200).json({ message: "Product deleted successfully!" });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 mongoose
   .connect(
     "mongodb+srv://behnamhamidi688:Iranashkani1991@cluster0.2lhiw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
